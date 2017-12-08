e34ebc36539e747b870579cb52871ed4691010ee
gdgunnars <gdg@gdg.is>
2017-12-08 16:52:10 +0000

    Started on displaying all the directories in a select menu on a html page using jinja

--- pictureframe_server/pictureframe_server.py
+++ pictureframe_server/pictureframe_server.py
@@ -4,6 +4,7 @@ import configparser
 
 from flask import Flask
 from flask import jsonify
+from flask import render_template
 from flask_cors import CORS
 
 IMG_BASE_DIR = "images"
@@ -12,10 +13,13 @@ CONFIG_FILE = "config.cfg"
 app = Flask(__name__)
 CORS(app)
 
+
 @app.route('/')
-def hello_world():
-    initial_config()
-    return 'Hello, World!'
+def index():
+    directories = get_base_directories()
+    directories.insert(0, "All");
+    return render_template('hello.html', target="All", directories=directories)
+
 
 def initial_config():
     config = configparser.ConfigParser()
@@ -24,6 +28,7 @@ def initial_config():
 
     with open(CONFIG_FILE, "w") as config_file:
         config.write(config_file)
+
 
 def read_config():
     config = configparser.ConfigParser()
@@ -39,16 +44,17 @@ def read_config():
 def config_page():
     return "ftp site for image uploads is: %s" % str(socket.gethostbyname(socket.gethostname()))
 
+
 @app.route('/get_images')
 def get_images():
     # returns a json object listing images for the currently selected image directory
 
     pictureframe_config = read_config()
     current_image_dir = pictureframe_config["DEFAULT"]["current_image_dir"]
-    directories = os.path.join(IMG_BASE_DIR, current_image_dir)
+    directories = [os.path.join(IMG_BASE_DIR, current_image_dir)]
     images = []
 
-    if (not os.path.isdir(os.path.join(IMG_BASE_DIR, current_image_dir))) or current_image_dir == "":
+    if (not os.path.isdir(directories[0])) or current_image_dir == "":
         directories = []
 
         for obj in os.listdir(IMG_BASE_DIR):
@@ -66,3 +72,15 @@ def get_images():
                 images.append(img_obj)
 
     return jsonify(images)
+
+
+def get_base_directories():
+    pictureframe_config = read_config()
+    current_image_dir = pictureframe_config["DEFAULT"]["current_image_dir"]
+    directories = []
+
+    for obj in os.listdir(IMG_BASE_DIR):
+
+        if os.path.isdir(os.path.join(IMG_BASE_DIR, obj)):
+            directories.append(obj)
+    return directories
